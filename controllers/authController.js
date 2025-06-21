6471e12e1343c6d5792585494a757195c94d6de3
Mapur2 <rupammodak1@gmail.com>
2025-06-21 17:16:09 +0530

    Secret to token

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,7 +14,7 @@ export const register = async (req, res) => {
     const hash = await bcrypt.hash(password, 10);
     const user = await User.create({ name, email, password: hash });
     const token = signToken({ id: user._id });
-    res.status(201).json({secret: token, user: { id: user._id, name: user.name, email: user.email } });
+    res.status(201).json({token, user: { id: user._id, name: user.name, email: user.email } });
   } catch (e) {
     console.log(e);
     res.status(500).json({ message: 'Registration failed', error: e.message });
